cceb4be048adbb6021eb209656da4a4a852ae42a
Olga Sokolova <olga.sokolova@extrawest.com>
2022-01-25 17:06:35 +0200

    Starts testing firebase rules

--- src/firebase/firebase.test.tsx
+++ src/firebase/firebase.test.tsx
@@ -1,37 +1,83 @@
-import { render } from "@testing-library/react";
-// Import firebase from your firebase init script:
-import { app } from "./firebaseConfig";
-import { getDatabase } from "firebase/database";
-import  { getStorage, ref } from "firebase/storage";
-import {
-	apps,
-	initializeTestApp, assertFails,assertSucceeds, clearFireStoreData} from "@firebase/rules-unit-testing"
+import { assertFails, initializeTestEnvironment } from "@firebase/rules-unit-testing";
+import { deleteObject, ref as storeRef } from "firebase/storage";
+import * as fs from "fs";
 
 const MY_PROJECT_ID = process.env.REACT_APP_FIREBASE_API_KEY;
-const userAuth = { uid: "user123", email: "user123@test.com" };
+const userAuth = { email: "user123@test.com", userId: "user123", };
 
-const getFireStore = (authUser) => getStorage(initializeTestApp({ projectId: MY_PROJECT_ID }))
+/*const getFireStore = (authUser: CurrentUser) =>
+	getStorage(initializeTestApp({ projectId: MY_PROJECT_ID, auth: authUser }));*/
 
+/*const alice = testEnv.authenticatedContext(
+	"alice",
+	userAuth
+);*/
+//let testEnv: RulesTestEnvironment;
 
 describe(
 	"Firestore security rules",
 	() => {
-		beforeEach(async() => {
-			await clearFireStoreData({projectId: MY_PROJECT_ID})
+
+		/*beforeEach(async () => {
+			await testEnv.clearStorage();
+			return;
 		});
-		/*afterEach(() => {
-			jest.restoreAllMocks();
-		  });*/
+
+		afterAll(async () => {
+			//await testEnv.cleanup();
+			return;
+		});*/
 
 		it(
-			"cannot read from collection",
-			() => {
-				const db = getFireStore(userAuth);
-				const testDoc = db.collection
-				const storage = getStorage(firebase.initializeTestApp({ projectId: MY_PROJECT_ID }))
-				const imagesRef = ref(storage, 'images');
-				const testDoc = db.collection("readonly").doc"testDoc2");
-				await firebase.assertFails(testDoc.set({ foo: "bar" }));
+			"cannot read from storage .unauthenticatedContext",
+			async () => {
+				const testEnv = await initializeTestEnvironment({
+					projectId: MY_PROJECT_ID,
+					storage: {
+						rules: fs.readFileSync(
+							"storage.rules",
+							"utf8"
+						),
+					},
+				});
+
+				const alice = testEnv.unauthenticatedContext();
+				const desertRef = storeRef(
+					alice.storage(),
+					"images/desert.jpg"
+				);
+				await assertFails(deleteObject(desertRef));
+			}
+		);
+
+		it(
+			"cannot read from storage",
+			async () => {
+				const testEnv = await initializeTestEnvironment({
+					projectId: MY_PROJECT_ID,
+					storage: {
+						rules: fs.readFileSync(
+							"storage.rules",
+							"utf8"
+						),
+					},
+				});
+
+				const alice = testEnv.authenticatedContext(
+					"alice",
+					userAuth
+				);
+				const createTestFile = (size: number) => Buffer.alloc(size);
+				/*const desertRef = storeRef(
+					alice.storage(),
+					"images/desert.jpg"
+				);
+				await assertSucceeds(getDownloadURL(desertRef));*/
+
+				await assertFails(storeRef.put(
+					createTestFile(500 * 1024 * 1024),
+					{ contentType: "image/png" }
+				).then());
 			}
 		);
 	}
