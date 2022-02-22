554fbd453ce443cb7cb7f16247a28efcb748ccd3
olga-sokolova-ew <olga.sokolova@extrawest.com>
2022-02-22 16:44:06 +0200

    Hides mock data for dev version

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,13 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import App from "./App/App";
 import "./index.css";
-import { worker } from "./mock/browser";
-import { isDevelopment } from "./utils/helper";
+//import { worker } from "./mock/browser";
+//import { isDevelopment } from "./utils/helper";
 
-if (isDevelopment()) {
+//for dev with mock server data
+/*if (isDevelopment()) {
 	worker.start();
-}
+}*/
 
 ReactDOM.render(
 	<StrictMode>
